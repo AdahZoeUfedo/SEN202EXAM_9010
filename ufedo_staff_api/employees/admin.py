fce7511d5ec2c141935ab8c32df6635e86a51577
veritas student_project <9010@veritas.edu.ng>
2025-06-28 11:20:24 +0100

    I updated admin.py

--- ufedo_staff_api/employees/admin.py
+++ ufedo_staff_api/employees/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import StaffBase, Manager, Intern
 
-# Register your models here.
+
+admin.site.register(StaffBase)
+admin.site.register(Manager)
+admin.site.register(Intern)
